424b5164d5f077d7aff9228c7d5814cfd17c1fa8
S.Y <zzhong0114@gmail.com>
2024-06-10 16:27:20 +0900

    askDTO »ý¼º

---
+++ src/main/java/ask/AskDTO.java
@@ -0,0 +1,46 @@
+package ask;
+
+import cmn.DTO;
+
+public class AskDTO extends DTO {
+		
+		private int askNo;
+		private int shopNo;
+		private String userId;
+		private String askState;
+		private String userAsk;
+		private String shopAnswer;
+		private String asnwerDate;
+		public int getAskNo() {
+			return askNo;
+		}
+		public void setAskNo(int askNo) {
+			this.askNo = askNo;
+		}
+		public int getShopNo() {
+			return shopNo;
+		}
+		public void setShopNo(int shopNo) {
+			this.shopNo = shopNo;
+		}
+		public String getUserId() {
+			return userId;
+		}
+		public void setUserId(String userId) {
+			this.userId = userId;
+		}
+		public String getAskState() {
+			return askState;
+		}
+		
+		
+		@Override
+		public String toString() {
+			return "AskDTO [askNo=" + askNo + ", shopNo=" + shopNo + ", userId=" + userId + ", askState=" + askState
+					+ ", userAsk=" + userAsk + ", shopAnswer=" + shopAnswer + ", asnwerDate=" + asnwerDate + "]";
+		}
+		
+		
+		
+	}
+
