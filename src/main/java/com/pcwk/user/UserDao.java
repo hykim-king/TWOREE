c224a6784156bc605f32d7bbd26061bbb67e32f1
C.H.Hwang <chanho0043@naver.com>
2024-06-10 16:19:00 +0900

    push

---
+++ src/main/java/com/pcwk/user/UserDao.java
@@ -0,0 +1,46 @@
+package com.pcwk.user;
+
+import java.util.List;
+
+import com.pcwk.ehr.cmn.DTO;
+import com.pcwk.ehr.cmn.WorkDlv;
+
+public class UserDao implements WorkDlv<UserDTO> {
+
+	@Override
+	public List<UserDTO> doRetrieve(DTO search) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int doSave(UserDTO param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int doUpdate(UserDTO param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int doDelete(UserDTO param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public UserDTO doSelectOne(UserDTO param) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int doUpdateReadCnt(UserDTO param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
