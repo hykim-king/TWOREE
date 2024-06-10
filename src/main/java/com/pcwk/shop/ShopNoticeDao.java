e330a16e1bab760693f6d9b5aa50242d5ba9fdc1
gieun <uki2022751011@gmail.com>
2024-06-10 16:52:15 +0900

    shop dao Ã߰¡

---
+++ src/main/java/com/pcwk/shop/ShopNoticeDao.java
@@ -0,0 +1,46 @@
+package com.pcwk.shop;
+
+import java.util.List;
+
+import com.pcwk.ehr.cmn.DTO;
+import com.pcwk.ehr.cmn.WorkDlv;
+
+public class ShopNoticeDao implements WorkDlv<ShopNoticeDTO> {
+
+	@Override
+	public List<ShopNoticeDTO> doRetrieve(DTO search) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int doSave(ShopNoticeDTO param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int doUpdate(ShopNoticeDTO param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int doDelete(ShopNoticeDTO param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ShopNoticeDTO doSelectOne(ShopNoticeDTO param) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int doUpdateReadCnt(ShopNoticeDTO param) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
