85f43f16bd0c72071ed65b9c9bb2c03302c3a2ca
M.W.Lee <dlandnjs43454@gmailcom>
2024-06-11 16:30:57 +0900

    DTO ¼öÁ¤

--- src/main/java/com/pcwk/review/reviewDTO.java
+++ src/main/java/com/pcwk/review/reviewDTO.java
@@ -4,34 +4,34 @@ import java.time.LocalDateTime;
 
 public class reviewDTO {
 	
-	private int review_no; 						//리뷰 고유 번호
-	private String shopNo; 						// 리뷰 작성받은 가게
-	private String userId; 						// 유저 아이디
-	private String reviewId; 					//리뷰 작성자 아이디
-	private LocalDateTime createDate; 			//리뷰 작성 날짜
-	private LocalDateTime updateDate; 			// 리뷰 수정 시간
-	private String reviewText; 					// 리뷰 내용
-	private int rating; 						// 점수 (1 ~5점)
+	private int reviewNo; 					//리뷰 고유 번호
+	private String shopNo; 					// 리뷰 작성받은 가게
+	private String reviewId; 				// 유저 아이디
+	private String userId; 					//리뷰 작성자 아이디
+	private String reviewWrtDate; 			//리뷰 작성 날짜
+	private String reviewModDate; 			// 리뷰 수정 시간
+	private String reviewContent; 			// 리뷰 내용
+	private int score; 						// 점수 (1 ~5점)
 	
-	public reviewDTO(int review_no, String shopNo, String userId, String reviewId, LocalDateTime createDate,
-			LocalDateTime updateDate, String reviewText, int rating) {
+	public reviewDTO(int reviewNo, String shopNo, String reviewId, String userId, String reviewWrtDate,
+		String reviewModDate, String reviewContent, int score) {
 		super();
-		this.review_no = review_no;
+		this.reviewNo = reviewNo;
 		this.shopNo = shopNo;
-		this.userId = userId;
 		this.reviewId = reviewId;
-		this.createDate = createDate;
-		this.updateDate = updateDate;
-		this.reviewText = reviewText;
-		this.rating = rating;
+		this.userId = userId;
+		this.reviewWrtDate = reviewWrtDate;
+		this.reviewModDate = reviewModDate;
+		this.reviewContent = reviewContent;
+		this.score = score;
 	}
 
-	public int getReview_no() {
-		return review_no;
+	public int getReviewNo() {
+		return reviewNo;
 	}
 
-	public void setReview_no(int review_no) {
-		this.review_no = review_no;
+	public void setReviewNo(int reviewNo) {
+		this.reviewNo = reviewNo;
 	}
 
 	public String getShopNo() {
@@ -42,14 +42,6 @@ public class reviewDTO {
 		this.shopNo = shopNo;
 	}
 
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
 	public String getReviewId() {
 		return reviewId;
 	}
@@ -58,46 +50,55 @@ public class reviewDTO {
 		this.reviewId = reviewId;
 	}
 
-	public LocalDateTime getCreateDate() {
-		return createDate;
+	public String getUserId() {
+		return userId;
 	}
 
-	public void setCreateDate(LocalDateTime createDate) {
-		this.createDate = createDate;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 
-	public LocalDateTime getUpdateDate() {
-		return updateDate;
+	public String getReviewWrtDate() {
+		return reviewWrtDate;
 	}
 
-	public void setUpdateDate(LocalDateTime updateDate) {
-		this.updateDate = updateDate;
+	public void setReviewWrtDate(String reviewWrtDate) {
+		this.reviewWrtDate = reviewWrtDate;
 	}
 
-	public String getReviewText() {
-		return reviewText;
+	public String getReviewModDate() {
+		return reviewModDate;
 	}
 
-	public void setReviewText(String reviewText) {
-		this.reviewText = reviewText;
+	public void setReviewModDate(String reviewModDate) {
+		this.reviewModDate = reviewModDate;
 	}
 
-	public int getRating() {
-		return rating;
+	public String getReviewContent() {
+		return reviewContent;
 	}
 
-	public void setRating(int rating) {
-		this.rating = rating;
+	public void setReviewContent(String reviewContent) {
+		this.reviewContent = reviewContent;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
 	}
 
 	@Override
 	public String toString() {
-		return "reviewDTO [review_no=" + review_no + ", shopNo=" + shopNo + ", userId=" + userId + ", reviewId="
-				+ reviewId + ", createDate=" + createDate + ", updateDate=" + updateDate + ", reviewText=" + reviewText
-				+ ", rating=" + rating + "]";
+		return "reviewDTO [reviewNo=" + reviewNo + ", shopNo=" + shopNo + ", reviewId=" + reviewId + ", userId="
+				+ userId + ", reviewWrtDate=" + reviewWrtDate + ", reviewModDate=" + reviewModDate + ", reviewContent="
+				+ reviewContent + ", score=" + score + "]";
 	}
-	
-	
-	
+
+
 	
 }
+
+
